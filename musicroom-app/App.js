6f8495bf08f56ff40012be8b2c831d1ede6eef0a
ayushyamitabh <ayushyamitabh@gmail.com>
2017-06-23 23:56:53 -0400

    Start

--- musicroom-app/App.js
+++ musicroom-app/App.js
@@ -7,17 +7,26 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake to open the developer menu.</Text>
+        <View style={styles.subcontainer}>
+          <Text>1</Text>
+          <Text>2</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  subcontainer: {
+    justifyContent: 'center',
+    borderColor: 'red',
+    borderWidth: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
